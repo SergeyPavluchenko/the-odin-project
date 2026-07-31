367109d8c5da35e3d605b7dc74427a1beae4b116
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2026-07-31 17:30:11 +0000

    added winning logic and a draw

--- Full-Stack-JavaScript/JavaScript/Tic-Tac-Toe/script.js
+++ Full-Stack-JavaScript/JavaScript/Tic-Tac-Toe/script.js
@@ -1,7 +1,15 @@
 // const board = document.querySelector('.table')
 const board = document.querySelector('.board')
+const win = document.querySelector('.winner')
+const startBtn = document.querySelector('.startBtn')
 
-let player = 'X'
+let player = 'X';
+let counterX = [];
+let counterO = [];
+let gameOver = false;
+
+
+
 
 const winComb = [
     [0, 1, 2],
@@ -14,30 +22,78 @@ const winComb = [
     [2, 4, 6]
 ]
 
-let counterX = []
-let counterO = []
+
+
+function winner(arr, move) {
+    return arr.every(index => move.includes(index))
+}
 
 for (let i = 0; i < 9; i++) {
     const cell = document.createElement('div')
     cell.classList.add('cell');
     cell.dataset.index = i;
     board.appendChild(cell)
+
 }
 
 function handleClick(e) {
-    const idx = Number(e.target.dataset.index)
-    e.target.textContent = player
-
-    if (player === 'X') {
-        player = 'O'
-        counterX.push(idx)
+    if (gameOver) {
+        return
     } else {
-        player = 'X'
-        counterO.push(idx)
+        const idx = Number(e.target.dataset.index)
+
+        if (e.target.textContent !== '') {
+            return
+        }
+
+        e.target.textContent = player
+
+
+        if (player === 'X') {
+            player = 'O'
+            counterX.push(idx)
+            const winnerX = winComb.some(comb => winner(comb, counterX))
+            if (winnerX === true) {
+                gameOver = true;
+                win.textContent = 'Player X win';
+                return
+            }
+        } else {
+            player = 'X'
+            counterO.push(idx)
+            const winnerO = winComb.some(comb => winner(comb, counterO))
+            if (winnerO === true) {
+                gameOver = true;
+                win.textContent = 'Player O win'
+                return
+            }
+        }
+
+        const total = counterX.length + counterO.length
+        if (total === 9) {
+            win.textContent = "Draw!";
+            gameOver = true;
+        }
+
 
     }
-    console.log(counterX);
-    console.log(counterO);
+
+
 }
 
+function handleStart() {
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => {
+        cell.textContent = ''
+    });
+
+    player = 'X'
+    counterX = []
+    counterO = []
+    gameOver = false
+    win.textContent = ''
+}
+
+
 board.addEventListener('click', handleClick)
+startBtn.addEventListener('click', handleStart)
