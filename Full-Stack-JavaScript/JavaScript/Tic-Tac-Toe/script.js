2028280144aa09e65f1234067f87288432d76c92
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2026-08-11 22:31:08 +0000

    add Format with Prettier, Rename winner, Change “Player X win” to “Player X wins,  Rename Start game button to Restart game, Add a check that the click target is a cell

--- Full-Stack-JavaScript/JavaScript/Tic-Tac-Toe/script.js
+++ Full-Stack-JavaScript/JavaScript/Tic-Tac-Toe/script.js
@@ -1,15 +1,14 @@
 // const board = document.querySelector('.table')
-const board = document.querySelector('.board')
-const win = document.querySelector('.winner')
-const startBtn = document.querySelector('.startBtn')
+const board = document.querySelector(".board");
+const win = document.querySelector(".winner");
+const startBtn = document.querySelector(".startBtn");
+const box = document.querySelector(".box")
+const click = document.querySelector(".click")
 
-let player = 'X';
+let player = "X";
 let counterX = [];
 let counterO = [];
 let gameOver = false;
-
-
-
 
 const winComb = [
     [0, 1, 2],
@@ -19,81 +18,86 @@ const winComb = [
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
-]
+    [2, 4, 6],
+];
 
-
-
-function winner(arr, move) {
-    return arr.every(index => move.includes(index))
+function hasWinningCombination(arr, move) {
+    return arr.every((index) => move.includes(index));
 }
 
 for (let i = 0; i < 9; i++) {
-    const cell = document.createElement('div')
-    cell.classList.add('cell');
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
     cell.dataset.index = i;
-    board.appendChild(cell)
-
+    board.appendChild(cell);
 }
 
+
 function handleClick(e) {
+
     if (gameOver) {
+        return;
+    }
+
+    if (!e.target.classList.contains("cell")) {
+        click.textContent = "Click on the board!"
         return
-    } else {
-        const idx = Number(e.target.dataset.index)
+    }
 
-        if (e.target.textContent !== '') {
-            return
-        }
+    click.textContent = ""
 
-        e.target.textContent = player
+    const idx = Number(e.target.dataset.index);
 
+    if (e.target.textContent !== "") {
+        return;
+    }
 
-        if (player === 'X') {
-            player = 'O'
-            counterX.push(idx)
-            const winnerX = winComb.some(comb => winner(comb, counterX))
-            if (winnerX === true) {
-                gameOver = true;
-                win.textContent = 'Player X win';
-                return
-            }
-        } else {
-            player = 'X'
-            counterO.push(idx)
-            const winnerO = winComb.some(comb => winner(comb, counterO))
-            if (winnerO === true) {
-                gameOver = true;
-                win.textContent = 'Player O win'
-                return
-            }
-        }
+    e.target.textContent = player;
 
-        const total = counterX.length + counterO.length
-        if (total === 9) {
-            win.textContent = "Draw!";
+    if (player === "X") {
+        player = "O";
+        counterX.push(idx);
+        const winnerX = winComb.some((comb) => hasWinningCombination(comb, counterX));
+        if (winnerX === true) {
             gameOver = true;
+            win.textContent = "Player X wins!";
+            click.textContent = ""
+            return;
         }
+    } else {
+        player = "X";
+        counterO.push(idx);
+        const winnerO = winComb.some((comb) => hasWinningCombination(comb, counterO));
+        if (winnerO === true) {
+            gameOver = true;
+            win.textContent = "Player O wins!";
+            click.textContent = ""
+            return;
+        }
+    }
 
 
+    const total = counterX.length + counterO.length;
+    if (total === 9) {
+        win.textContent = "Draw!";
+        gameOver = true;
     }
 
 
 }
 
 function handleStart() {
-    const cells = document.querySelectorAll('.cell')
-    cells.forEach(cell => {
-        cell.textContent = ''
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.textContent = "";
     });
 
-    player = 'X'
-    counterX = []
-    counterO = []
-    gameOver = false
-    win.textContent = ''
+    player = "X";
+    counterX = [];
+    counterO = [];
+    gameOver = false;
+    win.textContent = "";
 }
 
-
-board.addEventListener('click', handleClick)
-startBtn.addEventListener('click', handleStart)
+box.addEventListener("click", handleClick);
+startBtn.addEventListener("click", handleStart);
