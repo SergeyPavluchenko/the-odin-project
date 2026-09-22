0ab8629181946c253153c48023b1919be3c61461
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2026-09-22 21:00:37 +0000

    add Jest tests for basic cases and edge cases

--- Full-Stack-JavaScript/JavaScript/testing-practice/calculator.js
+++ Full-Stack-JavaScript/JavaScript/testing-practice/calculator.js
@@ -2,5 +2,32 @@ function subtract(a, b) {
     return a - b;
 }
 
-module.exports = subtract
 
+function createUser(name, age) {
+    return {
+        name: name,
+        age: age,
+    }
+}
+
+function getFruits() {
+    return ['apple', 'banana', 'orange']
+}
+
+function getUserName(user) {
+    return user.name;
+}
+
+function isEmpty() {
+    return false;
+}
+
+function findUser() {
+    return null
+}
+
+function getUserName1(user) {
+    return user.name;
+}
+
+module.exports = { subtract, createUser, getFruits, getUserName, isEmpty, findUser, getUserName1 }
--- Full-Stack-JavaScript/JavaScript/testing-practice/calculator.test.js
+++ Full-Stack-JavaScript/JavaScript/testing-practice/calculator.test.js
@@ -1,6 +1,79 @@
-const subtract = require('./calculator')
+const { subtract, createUser, getFruits, getUserName, isEmpty, findUser, getUserName1 } = require('./calculator')
+
+const resultFruits = getFruits()
+// const resultName = getUserName()
 
 
-test('відняти два числа', () => {
-    expect(subtract(10, 4)).toBe(6)
+describe('subtract', () => {
+
+    test('відняти два позитивних числа', () => {
+        expect(subtract(20, 5)).toBe(15)
+    })
+    test('відняти нуль', () => {
+        expect(subtract(15, 0)).toBe(15)
+    })
+    test('отримати відʼємний результат', () => {
+        expect(subtract(5, 10)).toBe(-5)
+    })
+    test('відняти від нуля', () => {
+        expect(subtract(0, 10)).toBe(-10)
+    })
+
+    test('відняти відʼємне число', () => {
+        expect(subtract(10, -5)).toBe(15)
+    })
 })
+
+test('перевірити обєкт', () => {
+    expect(createUser('Serhii', 36)).toEqual({
+        name: 'Serhii',
+        age: 36,
+    })
+})
+
+describe("getFruits", () => {
+
+    test("потрібний фрукт", () => {
+        expect(resultFruits).toContain('apple')
+    })
+    test("потрібний фрукт", () => {
+        expect(resultFruits).toContain('orange')
+    })
+})
+
+test('getUserName кидає помилку для null', () => {
+    expect(() => getUserName(null)).toThrow()
+})
+
+test('getUserName кидає помилку для undefined', () => {
+    expect(() => getUserName(undefined)).toThrow()
+})
+
+
+test('перевірка на false', () => {
+    expect(isEmpty()).toBeFalsy()
+})
+
+test('перевірка на відсутність користувача', () => {
+    expect(findUser()).toBeNull()
+})
+
+
+test("ім'я користувача порожнє", () => {
+    const user = { name: '' }
+
+    expect(getUserName1(user)).toBe('')
+}
+)
+test("ім'я користувача null", () => {
+    const user = { name: null }
+
+    expect(getUserName1(user)).toBeNull()
+}
+)
+test("ім'я користувача undefined", () => {
+    const user = {}
+
+    expect(getUserName1(user)).toBeUndefined()
+}
+)
